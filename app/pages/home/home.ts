c59c706369c33222429c811a0736b5bd4d98a657
Sarawut Tana <mynameissarawut@gmail.com>
2016-10-03 17:11:02 +0700

    update

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { OrderPage } from '../order/order';
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
   constructor(public navCtrl: NavController) {
+    
 
+  }
+  openOrderPage(){
+    this.navCtrl.push(OrderPage);
   }
 }
